2a791be29ab0aad9de5cc96514d27b283736c744
YangliuDebugger <46799407+YangliuDebugger@users.noreply.github.com>
2022-07-08 00:28:40 -0700

    07/08/2022 0:28

---
+++ 树/1123. Lowest Common Ancestor of Deepest Leaves.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.maxdepth = 0
+        self.ans = None
+        def dfs(node, depth):
+            if not node:
+                if depth > self.maxdepth:
+                    self.maxdepth = depth
+                return depth
+            max_depth1 = dfs(node.left, depth+1)
+            max_depth2 = dfs(node.right, depth+1)
+            if max_depth1 == max_depth2:
+                # 利用了深度优先的特点
+                if max_depth1 == self.maxdepth:
+                    self.ans = node
+            return max(max_depth1, max_depth2)
+        dfs(root, 0)
+        return self.ans
